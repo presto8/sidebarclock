db89256ed4870a4480d42f03e73a0d9ee1bb8af3
phunt <phunt@agnate2.had1.or.comcast.net>
2008-07-18 16:11:29 -0700

    Checking in compiled JS

--- common/gadget.all.js
+++ common/gadget.all.js
@@ -3098,6 +3098,11 @@ function adjustPositions() {
 }
 
 function adjustTimeToFit() {
+  if ( G.fontSize != 'Auto' ) {
+    gTime.fontsize = G.fontSize;
+    return;
+  }
+
   var maxWidth = 130;
   var maxHeight = getProperTimeHeight();
 
