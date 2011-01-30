fe9909d3c4463e56173cb9f8f99c5fde5a8b1ef3
Preston Hunt <me@prestonhunt.com>
2011-01-30 08:59:47 -0800

    Refactored to be more elegant

--- common/sun.js
+++ common/sun.js
@@ -1,4 +1,4 @@
-// SunriseSunset Class (2011-01-29)
+// SunriseSunset Class (2011-01-30)
 //   Implementation of http://williams.best.vwh.net/sunrise_sunset_algorithm.htm
 //
 //   Copyright (c) 2011, Preston Hunt <me@prestonhunt.com>
@@ -47,7 +47,7 @@
 //   var losangeles = new SunriseSunset( 2011, 1, 19, 34.05, -118.233333333 );
 //   etc.
 
-function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
+var SunriseSunset = function( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
     this.zenith = 90 + 50/60; //   offical      = 90 degrees 50'
                               //   civil        = 96 degrees
                               //   nautical     = 102 degrees
@@ -61,15 +61,17 @@ function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
 
     this.rising = true; // set to true for sunrise, false for sunset
     this.lngHour = this.longitude / 15;
+};
 
-    this.sin = function( deg ) { return Math.sin( deg * Math.PI / 180 ); };
-    this.cos = function( deg ) { return Math.cos( deg * Math.PI / 180 ); };
-    this.tan = function( deg ) { return Math.tan( deg * Math.PI / 180 ); };
-    this.asin = function( x ) { return (180/Math.PI) * Math.asin(x); };
-    this.acos = function( x ) { return (180/Math.PI) * Math.acos(x); };
-    this.atan = function( x ) { return (180/Math.PI) * Math.atan(x); };
+SunriseSunset.prototype = {
+    sin: function( deg ) { return Math.sin( deg * Math.PI / 180 ); },
+    cos: function( deg ) { return Math.cos( deg * Math.PI / 180 ); },
+    tan: function( deg ) { return Math.tan( deg * Math.PI / 180 ); },
+    asin: function( x ) { return (180/Math.PI) * Math.asin(x); },
+    acos: function( x ) { return (180/Math.PI) * Math.acos(x); },
+    atan: function( x ) { return (180/Math.PI) * Math.atan(x); },
 
-    this.getDOY = function() {
+    getDOY: function() {
         var month = this.utcMonth;
         var year = this.utcFullYear;
         var day = this.utcDay;
@@ -79,29 +81,29 @@ function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
         var N3 = (1 + Math.floor((year - 4 * Math.floor(year / 4 ) + 2) / 3));
         var N = N1 - (N2 * N3) + day - 30;
         return N;
-    };
+    },
 
-    this.approximateTime = function() {
+    approximateTime: function() {
         var doy = this.getDOY();
         if ( this.rising ) {
             return doy + ((6 - this.lngHour) / 24);
         } else {
             return doy + ((18 - this.lngHour) / 24);
         }
-    };
+    },
 
-    this.meanAnomaly = function() {
+    meanAnomaly: function() {
         var t = this.approximateTime();
         return (0.9856 * t) - 3.289;
-    };
+    },
 
-    this.trueLongitude = function() {
+    trueLongitude: function() {
         var M = this.meanAnomaly();
         var L = M + (1.916 * this.sin(M)) + (0.020 * this.sin(2 * M)) + 282.634;
         return L % 360;
-    };
+    },
 
-    this.rightAscension = function() {
+    rightAscension: function() {
         var L = this.trueLongitude();
         var RA = this.atan(0.91764 * this.tan(L));
         RA %= 360;
@@ -112,19 +114,19 @@ function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
         RA /= 15;
 
         return RA;
-    };
+    },
 
-    this.sinDec = function() {
+    sinDec: function() {
         var L = this.trueLongitude();
         var sinDec = 0.39782 * this.sin(L);
         return sinDec;
-    };
+    },
 
-    this.cosDec = function() {
+    cosDec: function() {
         return this.cos(this.asin(this.sinDec()));
-    };
+    },
 
-    this.localMeanTime = function() {
+    localMeanTime: function() {
         var cosH = (this.cos(this.zenith) - (this.sinDec() * this.sin(this.latitude))) 
             / (this.cosDec() * this.cos(this.latitude));
 
@@ -140,25 +142,25 @@ function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
             var T = H + RA - (0.06571 * t) - 6.622;
             return T;
         }
-    };
+    },
 
-    this.UTCTime = function() {
+    UTCTime: function() {
         var T = this.localMeanTime();
         var UT = T - this.lngHour;
         return UT % 24;
-    };
+    },
 
-    this.sunriseUtcHours = function() {
+    sunriseUtcHours: function() {
         this.rising = true;
         return this.UTCTime();
-    };
+    },
 
-    this.sunsetUtcHours = function() {
+    sunsetUtcHours: function() {
         this.rising = false;
         return this.UTCTime();
-    };
+    },
 
-    this.hoursRange = function( h ) {
+    hoursRange: function( h ) {
         if ( h >= 24 ) {
             return h - 24;
         } else if ( h < 0 ) {
@@ -166,17 +168,17 @@ function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
         } else {
             return h;
         }
-    };
+    },
 
-    this.sunriseLocalHours = function(gmt) {
+    sunriseLocalHours: function(gmt) {
         return this.hoursRange( gmt + this.sunriseUtcHours() );
-    };
+    },
 
-    this.sunsetLocalHours = function(gmt) {
+    sunsetLocalHours: function(gmt) {
         return this.hoursRange( gmt + this.sunsetUtcHours() );
-    };
+    },
 
-    this.isDaylight = function( utcCurrentHours ) {
+    isDaylight: function( utcCurrentHours ) {
         var sunriseHours = this.sunriseUtcHours();
         var sunsetHours = this.sunsetUtcHours();
 
@@ -199,8 +201,8 @@ function SunriseSunset( utcFullYear, utcMonth, utcDay, latitude, longitude ) {
         } 
 
         return false;
-    };
-}
+    }
+};
 
 function SunriseSunsetTest() {
     var testcases = {
