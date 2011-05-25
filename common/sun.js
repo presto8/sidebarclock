c64b597892de310969dff59e7fcdecab663c0d4b
Preston Hunt <me@prestonhunt.com>
2011-05-25 09:57:33 -0700

    Updating to tzdata 2011g

--- common/sun.js
+++ common/sun.js
@@ -1,45 +1,34 @@
-// SunriseSunset Class (2011-05-01)
+//   SunriseSunset Class (2011-05-02)
+//
+// OVERVIEW
+//
 //   Implementation of http://williams.best.vwh.net/sunrise_sunset_algorithm.htm
 //
-//   Copyright (c) 2011, Preston Hunt <me@prestonhunt.com>
-//   All rights reserved.
+// LICENSE
 //
-//   (BSD License)
+//   Copyright 2011 Preston Hunt <me@prestonhunt.com>
 //
-//   Redistribution and use in source and binary forms, with or without
-//   modification, are permitted provided that the following conditions
-//   are met:
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
 //
-//   Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the
-//   distribution.
+//       http://www.apache.org/licenses/LICENSE-2.0
 //
-//   The name of Preston Hunt may not be used to endorse or promote
-//   products derived from this software without specific prior written
-//   permission.
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 //
-//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-//   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-//   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-//   INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-//   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-//   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-//   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-//   OF THE POSSIBILITY OF SUCH DAMAGE.
+// DESCRIPTION
 //
 //   Provides sunrise and sunset times for specified date and position.
 //   All dates are UTC.  Year is 4-digit.  Month is 1-12.  Day is 1-31.
 //   Longitude is positive for east, negative for west. Latitude is
 //   positive for north, negative for south.
 //
-//   Sample usage:
+// SAMPLE USAGE
+//
 //   var tokyo = new SunriseSunset( 2011, 1, 19, 35+40/60, 139+45/60); 
 //   tokyo.sunriseUtcHours()      --> 21.8199 = 21:49 GMT
 //   tokyo.sunsetUtcHours()       --> 7.9070  = 07:54 GMT
